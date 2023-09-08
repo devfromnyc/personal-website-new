96d2dd73c372082b3ef8ba0e6d29be0adfa93684
devfromnyc <devfromnyc@gmail.com>
2023-09-08 02:50:01 -0400

    update: sidenav info

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -4,13 +4,14 @@ import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
 import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import HomeIcon from "@mui/icons-material/Home";
+import FilePresentIcon from "@mui/icons-material/FilePresent";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import PhoneIcon from "@mui/icons-material/Phone";
 import MenuIcon from "@mui/icons-material/Menu";
 
 const SideNav = () => {
@@ -36,24 +37,19 @@ const SideNav = () => {
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}>
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+        {["Home", "Portfolio", "Resume", "Contact Me"].map((text) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {text == "Home" ? (
+                  <HomeIcon />
+                ) : text == "Portfolio" ? (
+                  <LibraryBooksIcon />
+                ) : text == "Resume" ? (
+                  <FilePresentIcon />
+                ) : (
+                  <PhoneIcon />
+                )}
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
