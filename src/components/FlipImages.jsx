8ae2b9b8b348153970cc0883e55efd22232d200b
devfromnyc <devfromnyc@gmail.com>
2023-09-14 00:34:21 -0400

    update: created flipcard data file

--- src/components/FlipImages.jsx
+++ src/components/FlipImages.jsx
@@ -6,34 +6,7 @@ import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
 import { fadeLeft } from "../hooks/AnimationOptions";
-
-const itemData = [
-  {
-    img:
-      "https://images.unsplash.com/photo-1541877944-ac82a091518a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-    title: "Youtube Clone",
-    projectInfo: "A REACT Application that mirrors youtube!",
-  },
-  {
-    img:
-      "https://images.unsplash.com/photo-1579621970590-9d624316904b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-    title: "SAVR APP",
-    projectInfo:
-      "A fullstack REACT application that tracks how much money you save!",
-  },
-  {
-    img:
-      "https://images.unsplash.com/photo-1598791318878-10e76d178023?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-    title: "Todo List",
-    projectInfo: "Add items/tasks to a list that you'd like TODO in the future",
-  },
-  {
-    img:
-      "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-    title: "Fitness App",
-    projectInfo: "Project Info Coming Soon!",
-  },
-];
+import flipData from "../data/FlipCardData";
 
 const FlipImages = () => {
   const control = useAnimation();
@@ -99,7 +72,7 @@ const FlipImages = () => {
             pb: { xs: 12, lg: 24, xl: 32 },
           }}
         >
-          {itemData.map((item) => (
+          {flipData.map((item) => (
             <ImageListItem
               key={item.img}
               sx={{
---
+++ src/data/FlipCardData.jsx
@@ -0,0 +1,29 @@
+const flipData = [
+  {
+    img:
+      "https://images.unsplash.com/photo-1541877944-ac82a091518a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    title: "Youtube Clone",
+    projectInfo: "A REACT Application that mirrors youtube!",
+  },
+  {
+    img:
+      "https://images.unsplash.com/photo-1579621970590-9d624316904b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    title: "SAVR APP",
+    projectInfo:
+      "A fullstack REACT application that tracks how much money you save!",
+  },
+  {
+    img:
+      "https://images.unsplash.com/photo-1598791318878-10e76d178023?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    title: "Todo List",
+    projectInfo: "Add items/tasks to a list that you'd like TODO in the future",
+  },
+  {
+    img:
+      "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    title: "Fitness App",
+    projectInfo: "Project Info Coming Soon!",
+  },
+];
+
+export default flipData;
